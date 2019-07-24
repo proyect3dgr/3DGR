0bef601c914a28afe9c41be6a5f21345fc359241
rubander <ralexanderv89@gmail.com>
2019-07-24 19:03:07 +0200

    added better seed and fixed something else i dont really know

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/serverAuth', {useNewUrlParser: true})
+  .connect(process.env.BBDD_URL, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
