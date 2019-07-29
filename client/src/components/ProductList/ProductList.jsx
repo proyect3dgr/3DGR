198cde8401a57143c05e29c59eada760a598caf1
Gabriel B <gabelmo92@hotmail.com>
2019-07-29 12:42:18 +0200

    search bar

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../../SASS/Main.scss";
 import ProductCard from "../ProductCard/ProductCard";
 import AssetServices from "../../Services/assetServices";
 import Search from "../Search/Search";
@@ -8,9 +7,11 @@ export default class ProductList extends Component {
   constructor() {
     super();
     this.state = {
+      filterQuery: "",
       assetDetails: [{ author: "" }],
       isLoading: true,
       filteredAssets: [{ author: "" }]
+      
     };
     this.service = new AssetServices();
     this._isMounted = false;
@@ -33,15 +34,48 @@ export default class ProductList extends Component {
     this._isMounted = false;
   }
 
+
+
+  // handleInputChange = event => {
+  //   const query = event.target.value;
+
+  //   this.setState(prevState => {
+  //     const filteredData = prevState.data.filter(element => {
+  //       return element.name.toLowerCase().includes(query.toLowerCase());
+  //     });
+
+  //     return {
+  //       query,
+  //       filteredData
+  //     };
+  //   });
+  // };
+
+
+  filterProduct(e) {
+    
+    const filter = e.target.value;
+    
+    let filteredProducts = this.state.assetDetails.filter(product => {
+      return product.title.toLowerCase().indexOf(filter.toLowerCase()) > -1;
+    });
+
+    this.setState({
+      ...this.state,
+      filterQuery: filter,
+      filteredAssets: filteredProducts,
+    });
+  }
+
   render() {
     return (
       <section className="allProductList">
-        <div className="searchBar">
-          <Search filter={this.props.filter} />
+        <div className="searchBar"   >
+          <Search filterQuery={this.state.filterQuery} filterProductHandler={e => this.filterProduct(e)}/>
         </div>
 
         <div className="productList">
-          {this.state.assetDetails.map((asset, idx) => (
+          {this.state.filteredAssets.map((asset, idx) => (
             <ProductCard key={idx} {...asset} />
           ))}
         </div>
