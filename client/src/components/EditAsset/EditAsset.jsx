c1d9426d9d06cd621a4c4b2970ddd37113173c03
rubander <ralexanderv89@gmail.com>
2019-08-01 19:15:42 +0200

    EDITED

--- client/src/components/EditAsset/EditAsset.jsx
+++ client/src/components/EditAsset/EditAsset.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import assetServices from "../../Services/assetServices";
+import { Redirect } from "react-router-dom";
+
 
 export default class EditAsset extends Component {
   constructor(props) {
@@ -12,7 +14,7 @@ export default class EditAsset extends Component {
       image: "",
       model: "",
       size: "",
-      created: false,
+      edited: false,
       imgUploaded: false,
       modelUploaded: false
     };
@@ -51,7 +53,7 @@ export default class EditAsset extends Component {
         });
       })
       .then(response => {
-        this.setState({ created: true });
+        this.setState({ edited: true });
       })
       .catch(error => {
         console.log(error);
@@ -78,7 +80,7 @@ export default class EditAsset extends Component {
         });
       })
       .then(response => {
-        this.setState({ imgUploaded: false });
+        this.setState({ edited: true });
       })
       .catch(error => {
         console.log(error);
@@ -104,7 +106,7 @@ export default class EditAsset extends Component {
         });
       })
       .then(response => {
-        this.setState({ created: false });
+        this.setState({ edited: true });
       })
       .catch(error => {
         console.log(error);
@@ -170,6 +172,7 @@ export default class EditAsset extends Component {
   };
 
   render() {
+    if(this.state.edited === true) { return (<Redirect to={`/product/${this.props.match.params.id}`}/>)}
     return (
       <section className="edditAsset">
         <h1>Update your Asset</h1>
@@ -203,7 +206,7 @@ export default class EditAsset extends Component {
             </div>
           </div>
           <div className="infernoPart">
-            <button>Update Then!</button>
+            <button>Update Them!</button>
           </div>
         </form>
         
