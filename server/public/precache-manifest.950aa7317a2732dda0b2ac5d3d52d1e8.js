bde3da0f7ab5538eba596a3400418c2fc72e19ed
Gabriel B <gabelmo92@hotmail.com>
2019-08-01 19:00:38 +0200

    final final final

---
+++ server/public/precache-manifest.950aa7317a2732dda0b2ac5d3d52d1e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d89de535911a2b3227fc44c4b737b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8465facabbdc8951a44",
+    "url": "/static/css/2.ce8b4b48.chunk.css"
+  },
+  {
+    "revision": "86956d4a0b1c8940edd2",
+    "url": "/static/css/main.27535e32.chunk.css"
+  },
+  {
+    "revision": "e8465facabbdc8951a44",
+    "url": "/static/js/2.9d724183.chunk.js"
+  },
+  {
+    "revision": "86956d4a0b1c8940edd2",
+    "url": "/static/js/main.6bbf909f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "659814855c153eae136628781d19edd2",
+    "url": "/static/media/banner3.65981485.jpg"
+  },
+  {
+    "revision": "53c6c44c364c37b9d95afaa78fc8407e",
+    "url": "/static/media/bin.53c6c44c.png"
+  },
+  {
+    "revision": "0000ab503d9469f2dc1731c1d25cd92e",
+    "url": "/static/media/cart.0000ab50.png"
+  },
+  {
+    "revision": "07c57606c8a9866a71e0d46887a1fbbe",
+    "url": "/static/media/edit-button.07c57606.svg"
+  },
+  {
+    "revision": "9a5972e7f3cfaaab4ce54941227f082e",
+    "url": "/static/media/trailer.9a5972e7.mp4"
+  }
+]);
